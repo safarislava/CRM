945134e7b35ebfb39a15e1c66b6cb4ac56c36f60
safarislava <safarislava@gmail.com>
2026-08-02 20:01:42 +0300

    Add admin routes

---
+++ Backend/src/endpoint/admin/invitations/delete.rs
@@ -0,0 +1,19 @@
+use crate::endpoint::api_error::ApiError;
+use crate::model::admin::invitation_revocation::InvitationRevocation;
+use crate::model::task::contract::task::Task;
+use crate::state::AppState;
+use actix_web::{HttpResponse, web};
+use uuid::Uuid;
+
+pub async fn delete(
+    state: web::Data<AppState>,
+    path: web::Path<Uuid>,
+) -> Result<HttpResponse, ApiError> {
+    let token = path.into_inner();
+    InvitationRevocation::new(state.pool.clone(), token)
+        .done()
+        .await
+        .map_err(|e| ApiError::Internal(e.to_string()))?;
+
+    Ok(HttpResponse::NoContent().finish())
+}
---
+++ Backend/src/endpoint/admin/invitations/list.rs
@@ -0,0 +1,14 @@
+use crate::endpoint::api_error::ApiError;
+use crate::model::admin::invitations_list::InvitationsList;
+use crate::model::project::contract::json::Json;
+use crate::state::AppState;
+use actix_web::{HttpResponse, web};
+
+pub async fn get(state: web::Data<AppState>) -> Result<HttpResponse, ApiError> {
+    let data = InvitationsList::new(state.pool.clone())
+        .json()
+        .await
+        .map_err(|e| ApiError::Internal(e.to_string()))?;
+
+    Ok(HttpResponse::Ok().json(data))
+}
